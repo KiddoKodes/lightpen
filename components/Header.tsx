fef969f347862e530ca46c77317a099f1392aecc
kiddokodes <mabhirama@hotmail.com>
2022-03-01 16:09:30 +0530

    Got the homepage with header and poster

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,8 +3,24 @@ import React from 'react'
 
 const Header = () => {
     return (
-        <header>
-
+        <header className="flex justify-between p-5 max-w-7xl mx-auto">
+            <div className="flex items-center space-x-5">
+                <div className="flex items-center">
+                    <img src="/thunder.png" alt="Light Pen" className="w-16 object-contain cursor-pointer" />
+                    <Link href="/">
+                        <h1 className="font-bold text-2xl">Light Pen</h1>
+                    </Link>
+                </div>
+                <div className="hidden md:inline-flex items-center space-x-5">
+                    <h3>About</h3>
+                    <h3>Contact</h3>
+                    <h3 className="text-black bg-yellow-400 rounded-full px-4 py-1">Follow</h3>
+                </div>
+            </div>
+            <div className="flex items-center space-x-5 text-yellow-400">
+                <h3>Sign In</h3>
+                <h3 className="border border-yellow-400 rounded-full px-4 py-1 cursor-pointer">Get Started</h3>
+            </div>
         </header>
     )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,25 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import Header from '../components/header'
+import Header from '../components/Header'
 
 const Home: NextPage = () => {
   return (
-    <div className="">
+    <div className="bg-black min-h-screen text-white">
       <Head>
         <title>Light Pen</title>
       </Head>
       <Header />
+      <div className="flex  justify-between p-5 max-w-7xl mx-auto items-center shadow-md shadow-slate-300 py-8">
+        <div className="px-10 space-y-5">
+          <h1 className="text-5xl max-w-xl"><span className="text-yellow-400 underline">Light Pen</span> is a place for you and other folks to write,read and connect </h1>
+          <h2>It's easy and free to post your thinking on any topic and connect with millions of readers</h2>
+        </div>
+
+        <div>
+          <img src="/thunder.png" alt="LP" className="hidden  md:inline-flex h-36 w-56 lg:h-full" />
+        </div>
+      </div>
     </div>
   )
 }
